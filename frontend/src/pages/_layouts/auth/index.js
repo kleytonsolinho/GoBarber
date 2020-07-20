8226e3776da8a52dad177f0c989812e0d9d5140f
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-07-20 16:44:07 -0300

    globalstyles config

---
+++ frontend/src/pages/_layouts/auth/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Wrapper } from './styles';
+
+function AuthLayout({ children }) {
+  return (
+    <Wrapper>
+      {children}
+    </Wrapper>
+  );
+}
+
+export default AuthLayout;
+
+AuthLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
